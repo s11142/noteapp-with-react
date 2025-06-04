41278bc129e2afafabccbaa25de6212c6b00e619
s11142 <shotanagaizumi@gmail.com>
2025-06-04 09:42:20 +0900

    feat: SideBarの実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
+import { v4 as uuid } from "uuid";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  modDate: number;
 }
 
-export default App
+function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDelete = (id: string) => {
+    setNotes(notes.filter((note) => note.id !== id));
+    console.log(notes);
+  };
+
+  return (
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} onDelete={onDelete} notes={notes} />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,38 @@
+import type { Note } from "../App";
+import "./Sidebar.css";
+
+interface Props {
+  onAddNote: () => void;
+  onDelete: (id: string) => void;
+  notes: Note[];
+}
+
+const Sidebar = ({ onAddNote, onDelete, notes }: Props) => {
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>ノート</h1>
+        <button onClick={onAddNote}>追加</button>
+      </div>
+      <div className="app-sidebar-notes">
+        {notes.map((note) => (
+          <div className="app-sidebar-note" key={note.id}>
+            <div className="sidebar-note-title">
+              <strong>{note.title}</strong>
+              <button onClick={() => onDelete(note.id)}>削除</button>
+            </div>
+            <p>{note.content}</p>
+            <small>
+              {new Date(note.modDate).toLocaleDateString("ja-JP", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
